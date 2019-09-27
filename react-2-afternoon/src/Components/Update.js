84c788dfe17ddb27183e99f39eb6760372f8eb86
unknown <sutherlandscott21@gmail.com>
2019-09-26 19:34:35 -0600

    completed project

--- react-2-afternoon/src/Components/Update.js
+++ react-2-afternoon/src/Components/Update.js
@@ -39,20 +39,22 @@ class Update extends Component {
       <div className="update">       
         <div className="id-number">{data[this.state.index].id}/25</div>
             <div className="name">
-                <div className="first-name">{data[this.state.index].name.first}</div>
-                <div className="last-name"> {data[this.state.index].name.last}</div>
+                <div className="first-name">{`${data[this.state.index].name.first} ${data[this.state.index].name.last}` }</div>
              </div>
+             <br/>
              <div className="city-country">
-                 <span>From: </span>
+                 <span>From: &nbsp; </span>
                 <div className="city">{data[this.state.index].city}, </div>
                 <div className="country">{data[this.state.index].country}</div>
+                
             </div>
+            
             <div className="job-title">
-                <span>Job Title:</span>
+                <span>Job Title: &nbsp;</span>
                 <div className="title">{data[this.state.index].title}</div>
             </div>
             <div className="employer-company">
-                <span>Employer: </span>
+                <span>Employer: &nbsp;</span>
                 <div className="employer"> {data[this.state.index].employer}</div>
             </div>   
             <div className="favorite-movies">
@@ -63,13 +65,13 @@ class Update extends Component {
                 <div className="favorite-movies-three">3. {data[this.state.index].favoriteMovies[2]}</div>
             </div>
             <section className="button-bar">
-                <button className="white-buttons" onClick={() => this.handlePrevious()}>Previous</button>
+                <button className="white-buttons" onClick={() => this.handlePrevious()}> &lt; Previous</button>
                     <div className="blue-buttons"> 
                         <button id="edit-button">Edit</button>
                         <button id="delete-button">Delete</button>
                         <button id="new-button">New</button>
                     </div>
-                <button className="white-buttons" onClick={() => this.handleNext()}>Next</button>
+                <button className="white-buttons" onClick={() => this.handleNext()}> Next &gt;</button>
         </section>
       </div>
     )
